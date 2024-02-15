a4df2aff0fd60d891699b4bc450bb4135a01556c
HoNguyen <honguyen25121991@gmail.com>
2024-02-15 17:13:18 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/tesst.java
+++ src/main/java/vn/hoidanit/laptopshop/tesst.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-public class HelloWorldController {
+public class tesst {
     @GetMapping("/")
     public String index() {
         return "Chúc Mừng Năm Mới222dsadsaddadsadaffffffsdádsddadsasadsddda22111 2024";
