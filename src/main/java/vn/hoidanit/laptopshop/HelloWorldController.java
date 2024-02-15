06fe5719eedff6764fb1fa567c0bdab7dd549f5f
HoNguyen <honguyen25121991@gmail.com>
2024-02-15 16:49:08 +0700

    Build source base

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String index() {
+        return "Chúc Mừng Năm Mới 2024";
+    }
+
+}
