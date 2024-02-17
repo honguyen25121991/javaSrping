b4c431002afa5fbfe585771906aee33264125d13
HoNguyen <honguyen25121991@gmail.com>
2024-02-17 20:07:27 +0700

    Add mapping for GET /hello in HelloWorldController

--- src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloWorldController {
+    // Write a mapping for GET /hello
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+
     @GetMapping("/")
     public String index() {
         return "Chúc Mừng Năm Mới22222222222111 2024";
