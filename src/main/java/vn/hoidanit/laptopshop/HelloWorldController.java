462d50bd17b5d97e268209620a09d90a4e52d5c6
HoNguyen <honguyen25121991@gmail.com>
2024-02-17 20:15:13 +0700

    zzz

--- src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
@@ -5,15 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloWorldController {
-    // Write a mapping for GET /hello
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World";
-    }
-
     @GetMapping("/")
     public String index() {
-        return "Chúc Mừng Năm Mới222dsadsadsadsddda22111 2024";
+        return "Chúc Mừng Năm  2024";
     }
 
 }
