bbd63fee3c627886d9439ed835ca94a1dce3c229
HoNguyen <honguyen25121991@gmail.com>
2024-02-18 16:06:05 +0700

    Add launch configuration and new service class

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,21 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		ApplicationContext context = SpringApplication.run(LaptopshopApplication.class, args);
+
+		// Get the names of all the beans in the application context
+		String[] beanNames = context.getBeanDefinitionNames();
+
+		// Print the names of all the beans
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 
 }
---
+++ src/main/java/vn/hoidanit/laptopshop/services/MyServices.java
@@ -0,0 +1,12 @@
+package vn.hoidanit.laptopshop.services;
+
+import org.springframework.stereotype.Service;
+
+public class MyServices {
+    @Service
+    public class MyService {
+        public String getHello() {
+            return "Hello";
+        }
+    }
+}
