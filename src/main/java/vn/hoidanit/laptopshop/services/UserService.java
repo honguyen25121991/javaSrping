1b0b1de617eb9a692409daac1658d962497c7a25
HoNguyen <honguyen25121991@gmail.com>
2024-03-12 17:50:45 +0700

    Add dependencies and update controller and service classes

---
+++ src/main/java/vn/hoidanit/laptopshop/services/UserService.java
@@ -0,0 +1,11 @@
+package vn.hoidanit.laptopshop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserService {
+    public String handleHello() {
+        return "Hello form User Service";
+    }
+}
